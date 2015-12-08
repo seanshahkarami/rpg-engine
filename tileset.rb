c33a1566cf61af78cf7f3ce75b685e0bc194d078
Sean Shahkarami <sean.shahkarami@gmail.com>
2015-12-07 23:43:17 -0600

    corrected border issue

--- tileset.rb
+++ tileset.rb
@@ -1,7 +1,7 @@
 class Tileset
   
   def initialize(engine, filename)
-    @tiles = Gosu::Image.load_tiles(engine, filename, 32, 32, false)
+    @tiles = Gosu::Image.load_tiles(engine, filename, 32, 32, true)
   end
   
   def tile(index)
